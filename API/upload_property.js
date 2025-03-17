b0129ae0e859d30d6dc92c10e8a364af00c3878d
dp <codewithdp2073@gmail.com>
2025-03-17 13:07:37 +0530

    upload property API ready

---
+++ API/upload_property.js
@@ -0,0 +1,38 @@
+const connectDB = require("../DB/connectDB");
+
+async function upload_property(req, res) {
+    try {
+        const db = await connectDB();
+        const collection = db.collection("Property");
+
+
+        const { property_Id, title, description, location, address, zipCode, propertyType, category, size, price, bedrooms, bathrooms, images: [], propertyIdentityType, propertyProof: [], amenities: [], identityType, identityId, } = req.body;
+
+        await collection.insertOne({
+            property_Id,
+            title,
+            description,
+            location,
+            address,
+            zipCode,
+            propertyType,
+            category,
+            size,
+            price,
+            bedrooms,
+            bathrooms,
+            images: [],
+            propertyIdentityType,
+            propertyProof: [],
+            amenities: [],
+            identityType,
+            identityId,
+        });
+        return res.status(200).json({ message: "Property uploaded Successfully" });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal server Error" });
+    }
+}
+
+module.exports = { upload_property };
