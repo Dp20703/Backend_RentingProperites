11597c8a4fc1dcf280582117abcf7a0780bc5d47
dp <codewithdp2073@gmail.com>
2025-03-17 15:16:23 +0530

    response key added in send feedback API

--- API/send_feedback.js
+++ API/send_feedback.js
@@ -4,13 +4,14 @@ async function send_feedback(req,res) {
     try {
         const db = await connectDB();
         const collection = db.collection("Feedback");
-        const { feedback_Id, user_Id, owner_Id, feedbackDate, rating, comments } = req.body;
+        const { feedback_Id, user_Id, owner_Id, feedbackDate, rating,response, comments } = req.body;
         await collection.insertOne({
             feedback_Id,
             user_Id,
             owner_Id,
             feedbackDate,
             rating,
+            response,
             comments,
         });
         return res.status(200).json({ message: "Feedack sent Successfully" });
