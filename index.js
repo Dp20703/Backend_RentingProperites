3e627abebd4528eacd429e1e0db8725f92df7ef9
dp <codewithdp2073@gmail.com>
2025-03-17 13:07:25 +0530

    register user and register owner APIs ready

---
+++ index.js
@@ -0,0 +1,28 @@
+const connectDB = require('./DB/connectDB');
+const express = require("express");
+const cors = require("cors");
+const { register_user } = require('./API/register_user');
+const { register_owner } = require('./API/register_owner');
+const { upload_property } = require('./API/upload_property');
+
+
+//connecting to database:
+connectDB();
+
+//middlewares:
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+//routes:
+app.post("/register_user", register_user);
+app.post("/register_owner", register_owner);
+app.post("/upload_property", upload_property);
+
+
+
+//port:
+const PORT = 8000;
+app.listen(PORT);
+console.log(`Server is listing on port http:localhost:${PORT}`)
